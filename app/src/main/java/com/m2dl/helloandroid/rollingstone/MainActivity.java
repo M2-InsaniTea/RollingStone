c7137494060652ab9227c0b3cce592b9d3610082
Abel <krusnik02@hotmail.fr>
2016-01-28 10:19:47 +0100

    added application main activity and screen

---
+++ app/src/main/java/com/m2dl/helloandroid/rollingstone/MainActivity.java
@@ -0,0 +1,31 @@
+package com.m2dl.helloandroid.rollingstone;
+
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else {
+            hideSystemUI();
+        }
+        setContentView(R.layout.activity_main);
+    }
+
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+
+}
